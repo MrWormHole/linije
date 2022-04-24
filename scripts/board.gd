781f10119d156eb549f720c68bc63b8831821602
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-04-24 01:02:32 +0100

    drawing the board

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,19 +1,32 @@
 extends Node
 
-export var width: int
-export var height: int
+export var row_count: int
+export var col_count: int
 export var x_start: int
 export var y_start: int
-export var offset: int
 
-var possible_cells = [
-	preload("res://scenes/black_cell.tscn"),
-	preload("res://scenes/white_cell.tscn"),
-]
+const MAX_WIDTH = 576 # 9 X 64
+const MAX_HEIGHT = 1024 # 16 X 64
+const CELL_SIZE = 32.0
+var ash_gray = Color("#CCD7C5") # could be a background
+var misty_rose = Color("#EFD2CB") # background
+var camel = Color("#C7A27C")
+var mystic = Color("#D65780")
+var dark_salmon = Color("#EE9480")
+
+var cell_prefab = preload("res://scenes/cell.tscn")
 var all_cells: Array = []
+var all_colors: Array = [
+	camel,
+	mystic,
+	dark_salmon,
+]
 
 func _ready() -> void:
 	randomize()
+	# VisualServer.set_default_clear_color(ash_gray)
+	var cells = make_board(row_count, col_count)
+	spawn_cells(cells)
 
 func make_board(rows: int, cols: int) -> Array:
 	var arr: Array = []
@@ -23,17 +36,21 @@ func make_board(rows: int, cols: int) -> Array:
 			arr[i].append(null)
 	return arr
 
-func spawn_cells(rows: int, cols: int) -> void:
-	for i in range(rows):
-		for j in range(cols):
-			var rand: int = floor(rand_range(0, possible_cells.size()))
-			var cell = possible_cells[rand].instance()
+func spawn_cells(cells: Array) -> void:
+	for i in range(cells.size()):
+		for j in range(cells[i].size()):
+			var cell = cell_prefab.instance()
+			var rand: int = int((rand_range(0, all_colors.size())))
+			cell.color = all_colors[rand]
+			cell.width =  CELL_SIZE
+			cell.height = CELL_SIZE
+			var offset: float = calculate_offset()
+			cell.x1 = float(x_start + (j * offset))
+			cell.y1 = float(y_start + (i * offset)) 
 			add_child(cell)
-			cell.position = board_to_pixel(i, j)
 
-func board_to_pixel(row: int, col: int) -> Vector2:
-	var new_x: int = x_start + col * offset
-	var new_y: int = y_start - row * offset
-	return Vector2(new_x, new_y)
-
+func calculate_offset() -> float:
+	var offset: float = (MAX_WIDTH - 2 * x_start - CELL_SIZE * col_count) / col_count
+	offset += CELL_SIZE
+	return offset			
 	
--- scripts/cell.gd
+++ scripts/cell.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
-export var color: String
+var color: Color
+var x1: float
+var y1: float
+var width: float
+var height: float
 
-func _ready:
+func _ready() -> void:
   pass
+
+func _draw() -> void:
+  draw_rect(Rect2(x1, y1, width, height), color, false, 3.0)
+
+func _to_string() -> String:
+  return "x1: {0}, y1: {1}, width: {2}, height: {3}".format([x1, y1, width, height])
