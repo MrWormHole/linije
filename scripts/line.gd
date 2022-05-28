b38fb42e958a47daf65e809717acd75192c11434
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-05-28 16:51:10 +0100

    added tween for the lines

--- scripts/line.gd
+++ scripts/line.gd
@@ -5,20 +5,25 @@ var size: float
 var color: Color
 var draw_start_point: Vector2
 var draw_end_point: Vector2
+var interpolation_seconds: float = 0.5
 
 onready var cell: Node
-
-### THESE ARE FOR DEBUGGING PURPOSE ONLY
-var timer = 0
-var wait_time = 2
-var switch = false
-### THESE ARE FOR DEBUGGING PURPOSE ONLY
+onready var tween: Node
 
 func _on_cell_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		print("Clicked")
+		
+		if int(rad2deg(self.rotation)) % 90 != 0:
+			return
+		
+		var current_rotation: float = self.rotation
+		var next_rotation: float = self.rotation + deg2rad(90)
+		tween.interpolate_property(self, "rotation", current_rotation, next_rotation, interpolation_seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
 
 func _ready() -> void:
+	tween = get_node("tween")
 	cell = get_parent()
 	size = cell.size
 	color = cell.color
@@ -27,35 +32,12 @@ func _ready() -> void:
 	draw_end_point = draw_start_point + Vector2(size, size)	
 
 func _draw() -> void:
+	pass
 	draw_line(draw_start_point, draw_end_point, color, thickness)
 
 
 func _process(delta: float) -> void:
-	timer+=delta
-	if timer>wait_time:
-		switch = true
-		timer = 0
-
-	if !switch:
-		return	
-
-	self.rotation += PI * delta
-	draw_start_point = draw_start_point.rotated(PI * delta)
-	draw_end_point = draw_end_point.rotated(PI * delta)
-
-	if relative_to_global_point(draw_start_point).y < cell.transform.origin.y - size / 2 || relative_to_global_point(draw_start_point).y > cell.transform.origin.y + size / 2:
-		draw_start_point.y = global_to_relative_point(cell.transform.origin).y
-	if relative_to_global_point(draw_start_point).x < cell.transform.origin.x - size / 2 || relative_to_global_point(draw_start_point).x > cell.transform.origin.x + size / 2:
-		draw_start_point.x = global_to_relative_point(cell.transform.origin).x
-
-	if relative_to_global_point(draw_end_point).y < cell.transform.origin.y - size / 2 || relative_to_global_point(draw_end_point).y > cell.transform.origin.y + size / 2:
-		draw_end_point.y = global_to_relative_point(cell.transform.origin).y
-	if relative_to_global_point(draw_end_point).x < cell.transform.origin.x - size / 2 || relative_to_global_point(draw_end_point).x > cell.transform.origin.x + size / 2:
-		draw_end_point.x = global_to_relative_point(cell.transform.origin).x
-
-	
-	update()
-	switch = false
+	pass
 
 func relative_to_global_point(relative_point: Vector2) -> Vector2:
 	# take the relative  point and get the global point, use cell's origin position
