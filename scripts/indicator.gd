44207b68ae4379e3fd912740b0e5e255260be0ab
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-06-04 15:33:31 +0100

    indicator states

--- scripts/indicator.gd
+++ scripts/indicator.gd
@@ -3,25 +3,29 @@ extends Node2D
 var size: float
 var color: Color
 var draw_point: Vector2
+var current_state: int
+
+enum State { 
+  UNKNOWN,
+  LOCKED, 
+  UNLOCKED,
+}
 
 onready var label: Node
 
 func _ready() -> void:
 	draw_point = Vector2(-size/2, -size/2)
+	current_state = State.UNKNOWN
 	label = get_node("label")
 	label.rect_position += Vector2(-size/2, -size/2)
 
 func _draw() -> void:
-	draw_circle_arc(draw_point, size/2, 0, 360)
+	draw_circle_arc(draw_point, size/2, 0, 360, color)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func draw_circle_arc(center: Vector2, radius: float, angle_from: float, angle_to: float) -> void:
+func draw_circle_arc(center: Vector2, radius: float, angle_from: float, angle_to: float, fill_color: Color) -> void:
 	var point_count: int = 360
 	var points_arc: PoolVector2Array = PoolVector2Array([center])
-	var colors: PoolColorArray = PoolColorArray([color])
+	var colors: PoolColorArray = PoolColorArray([fill_color])
 
 	for i in range(point_count + 1):
 		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / point_count - 90)
